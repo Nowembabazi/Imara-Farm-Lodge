442f4a37f5fc11795768645ae9c7e202cb5205be
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-06-27 13:33:30 +0300

    about.main js files

--- src/component/About/Main.js
+++ src/component/About/Main.js
@@ -114,7 +114,7 @@ const Main = () => {
                     </div>
                 </div>
             </section>
-            <section className="feature-area2 p-relative fix" style={{background: "#f7f5f1"}}>
+            {/* <section className="feature-area2 p-relative fix" style={{background: "#f7f5f1"}}>
                  <div className="animations-02"><img src="assets/img/bg/an-img-02.png" alt="contact-bg-an-05"/></div>
                 <div className="container">
                     <div className="row justify-content-center align-items-center">
@@ -132,15 +132,15 @@ const Main = () => {
                                 </h2>                             
                                 </div>
                                 <p>Vestibulum non ornare nunc. Maecenas a metus in est iaculis pretium. Aliquam ullamcorper nibh lacus, ac suscipit ipsum consequat porttitor.Aenean vehicula ligula eu rhoncus porttitor. Duis vel lacinia quam. Nunc rutrum porta ex, in imperdiet tortor feugiat at.</p>
-                                <p>Cras finibus laoreet felis et hendrerit. Integer ligula lorem, finibus vitae lorem at, egestas consectetur urna. Integer id ultricies elit. Maecenas sodales nibh, quis posuere felis. In commodo mi lectus venenatis metus eget fringilla. Suspendisse varius ante eget.</p>
+                                <p>Cras finibus laoreet felis et hendrerit. Integer ligula lorem, finibus vitae lorem at, egestas consectetur urna. Integer id ultricies elit. Maecenas sodales nibh, quis posuere felis. In commodo mi lectus venenatis metus eget fringilla. Suspendisse varius ante eget.</p> */}
                                 {/* <div className="slider-btn mt-15">                                          
                                     <Link to ="/about" className="btn ss-btn smoth-scroll">Discover More</Link>				
                                 </div> */}
-                            </div>
+                            {/* </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
             {/* <section id="blog" className="blog-area p-relative fix pt-90 pb-90">
                  <div className="animations-02"><img src="assets/img/bg/an-img-06.png" alt="contact-bg-an-05"/></div>
                 <div className="container">
